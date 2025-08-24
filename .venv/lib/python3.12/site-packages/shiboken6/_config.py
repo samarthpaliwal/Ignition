57cc982c48565a77ae7d77509dfdfa221ac67a9e
samarthpaliwal <paliwalsamarth39@gmail.com>
2025-08-24 00:11:48 -0700

    Add large code

---
+++ .venv/lib/python3.12/site-packages/shiboken6/_config.py
@@ -0,0 +1,12 @@
+shiboken_library_soversion = "6.9"
+
+version = "6.9.1"
+version_info = (6, 9, 1, "", "")
+
+__build_date__ = '2025-05-31T17:32:14+00:00'
+
+
+
+
+__setup_py_package_version__ = '6.9.1'
+__qt_macos_min_deployment_target__ = '12'
